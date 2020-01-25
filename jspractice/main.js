2314ea65bf278d47d31d2bf7f3d0cf13b3c4cf63
AndrewThomasW <54901855+AndrewThomasW@users.noreply.github.com>
2020-01-25 17:39:12 -0600

    Update main.js

--- jspractice/main.js
+++ jspractice/main.js
@@ -1,165 +1,343 @@
-{
-    /**
-     * reverseArray produces new Array
-     * @param {Array} arg 
-     */ 
-    function reverseArray(arg){
-         let currentArray = [];
-         let lastIndex = arg.length-1;
-         const reverse = function (){
-             for(index in arg)
-                 currentArray.push(arg[lastIndex--])
-             
-             return currentArray;
-         }
-         return reverse.apply();
+function deepEqual(obj1, obj2) {
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+      return false;
     }
- 
-    /**
-     * reverseArrayInPlace rearranges element in the same memory location
-     * @param {Array} arg 
-     */
-    function reverseArrayInPlace(arg){
-      const reverse = function (){
-          for(index in arg){
-              //Swaps values until it reaches the middle of the array
-              if(Math.round((arg.length)/2) == index) break;
-              
-              //Computes the last  value {swapIndex} and current value {index} to swap
-              let swapIndex = (arg.length - 1) - index
-              let temp = arg[index]
-              arg[index] = arg[swapIndex]
-              arg[swapIndex] = temp      
-          }
-          return arg
+    if (typeof (obj1) !== typeof (obj2)) {
+      return false;
+    }
+
+    if (typeof (obj1) === "object") {
+      for(let key in obj1) {
+        return deepEqual(obj1[key], obj2[key]);
       }
-      return reverse.apply()
+    } else {
+      if (obj1 === obj2) {
+        return true;
+      }
+      else
+      {
+        return false;
     }
- 
+
+  }
+}
+
+  let student = {
+    here : {
+      is : "lam"
+    },
+    object : 2
+  };
+
+  describe("deepEqual", function () {
+    it("deepEqual(obj, obj) -> true", function () {
+      assert.equal(deepEqual(student, student), true);
+    });
+
+    it("deepEqual(obj, notequal) -> false", function () {
+        assert.equal(deepEqual(student, {
+          here : {
+            is : "Jim"
+          },
+          object : 2
+        }), false);
+      });
+});
+
+
+/*.............................*/
+(function () {
+    "use strict";
+  
     /**
-     * 
-     * @param {Array} array 
+     *
+     * @param arr
+     * @returns {[]}
      */
-    function listToArray(array){
-         let filteredArray = function processArray(){     
-             let arrayValues = [];     
-             array.forEach(function(value,index){
-                 if(index >= 1){
-                     arrayValues.push(value)
-                 }
-             })
-             return arrayValues;
-         }       
- 
-         return (function(){
-             if(array.length == 1){
-                 return {
-                     value : array[0],
-                     rest: null
-                 }
-             }else{
-                 return {
-                     value : array[0],
-                     rest: listToArray(filteredArray.apply())
-                 }
-             }
-         }())
+    function reverseArray(arr) {
+      let newArray = [];
+      for (let i = 0; i < arr.length; i++) {
+        newArray[i] = arr[arr.length - i - 1];
+      }
+      return newArray;
     }
- 
-    function deepEqual(arg1,arg2){
-         /** Test is valid object*/
-         const validate = function(argKey1,argKey2){
-             const test = function (){
-                 //checks for empty / null context
-                 if(this.length == 0){
-                     return false;
-                 }     
-                 return (typeof this == "object")
-             }
-             return function(){
-                 
-                 if(test.apply(argKey1) && test.apply(argKey2)){
-                     //Check keys first
-                     if((argKey1.toString() === argKey2.toString())){
-                         let flag = false;
-                         //Check length of both arrays
-                         if(argKey1.length == argKey2.length){
-                             return true;
-                         }else{
-                             return false;
-                         }
-                     }else{
-                         return false;
-                     }
-                 }else{
-                     return false;
-                 } 
-             }
-         }
- 
-        const predicate = function(){   
-            const argKey1 = Object.keys(arg1 == null?'':arg1)
-            const argKey2 = Object.keys(arg2 == null?'':arg2) 
-            let flag = true;
-            return function (){
-               
-               if(validate(argKey1,argKey2).apply()){
-                 
-                 for(key of argKey1){
-                     arg1[key] === arg2[key] ? flag : flag = false;
-                 }
- 
-                 return flag;
-               }else{
-                   return false;
-               }       
-            }
+  
+    /**
+     *
+     * @param arr
+     * @returns {*}
+     */
+    function reverseArrayInPlace(arr) {
+      for (let i = 0; i < arr.length / 2; i++) {
+        let tmp = arr[i];
+        arr[i] = arr[arr.length - i - 1];
+        arr[arr.length - i - 1] = tmp;
+      }
+      return arr;
+  
+    }
+  
+    /**
+     *
+     */
+    Array.prototype.reverseArray = function () {
+      let i = 0;
+      let middle = Math.floor(this.length / 2);
+      let temp = null;
+  
+      for (let i = 0; i < middle; i += 1) {
+        temp = this[i];
+        this[i] = this[n - 1 - i];
+        this[n - 1 - i] = temp;
+      }
+    };
+  
+    describe("reverse", function () {
+      it("reverseArray(Z,Y,X) returns XYZ", function () {
+        assert.equal(reverseArray(["Z","Y","X"]).toString(), ["X","Y","Z"]);
+      });
+  
+      it("reverseArrayInPlace(Z,Y,X) returns XYZ", function () {
+        assert.equal(reverseArrayInPlace(["Z","Y","X"]).toString(), ["X","Y","Z"]);
+      });
+  
+    });
+  
+  }());
+
+  /*.......................................*/
+  (function () {
+    "use strict"
+     /**
+      *
+      * @param number
+      * @param list
+      * @returns {{}}
+      */
+     function prepend(number, list) {
+       let obj = {};
+       obj.value = number;
+       obj.rest = list;
+       return obj;
+     }
+   
+     console.log(prepend(100, prepend(50, null)).value);
+   
+     describe("prepend", function () {
+       it("prepend(10, prepend(20, null))", function () {
+         assert.equal(prepend(100, prepend(50, null)).value, 100);
+       });
+   
+     });
+   
+   }());
+   /*..................................................*/
+   (function() {
+    "use strict";
+  
+    /**
+     *
+     * @param arr
+     * @returns {{rest: null}}
+     */
+  
+    function arrayToList(arr) {
+      let retVal = {
+        rest : null
+      };
+  
+      for (let i = 0; i < arr.length; i++) {
+  
+        if (retVal.rest === null) { // if its first element in list
+          retVal.value = arr[i]; // dynamically adding value attribute to
+          // retVal object
+          retVal.rest = {
+  
+            rest : null
+          };
+        } else {
+          let counter = retVal;
+          while (counter.rest !== null) {
+            counter = counter.rest;
+          }
+          counter.value = arr[i];
+          counter.rest = {
+  
+            rest : null
+          };
+  
         }
- 
-        return predicate.apply()
+  
+      }
+  
+      return retVal;
+    };
+    console.log();
+  
+    /**
+     *
+     * @param list
+     * @returns {[]}
+     */
+  
+    function listToArray(list) {
+  
+      let arr = [];
+  
+      while (list !== null) {
+        if (list.value !== undefined)
+          arr.push(list.value);
+        list = list.rest;
+      }
+  
+      return arr;
+  
     }
- 
- 
- 
- 
-    const user = {
-        'id': 1,
-        'name':'andrew thomas',
-        'age': 28,
-        'color':'brown'
+  
+  
+    describe("array -> List", function () {
+      it("arrayToList([ 1, 2, 3 ]", function () {
+        assert.equal(arrayToList([ 1, 2, 3 ]).toString(),"[object Object]")
+      })
+  
+      it("listToArray({\n" +
+        "        value : 1,\n" +
+        "        rest : {\n" +
+        "          value : 2,\n" +
+        "          rest : {\n" +
+        "            value : 3,\n" +
+        "            rest : null\n" +
+        "          }\n" +
+        "        }\n" +
+        "\n" +
+        "      } -> 123", function () {
+        assert.equal(listToArray({
+          value : 1,
+          rest : {
+            value : 2,
+            rest : {
+              value : 3,
+              rest : null
+            }
+          }
+  
+        }).toString(),"1,2,3")
+      })
+  
+    });
+  }());
+
+  /*........................................*/
+
+  (function () {
+    "use strict";
+  
+  
+    /**
+     *
+     * @param list
+     * @param index
+     * @returns {null|*}
+     */
+    function nth(list, index) {
+      let i = 0;
+      while (list != null && list.value != null) {
+        if (i == index) {
+          return list.value;
+        }
+        list = list.rest;
+        i++;
+      }
+      return null;
     }
- 
-    const user1 = {
-        'id': 1,
-        'name':'andrew thomas',
-        'age': 28,
-        'color':'brown'
+  
+  
+    /**
+     *
+     * @param list
+     * @param index
+     * @returns {*}
+     */
+    function recNth(list, index) {
+      if (index === 0) {
+        return list.value;
+      }
+      return recNth(list.rest, --index);
+  
     }
- 
- 
-    console.log(listToArray([1,2,3]));
-    console.log("Deep Equal",deepEqual(user,user1).apply());
-    console.log("Reverse Array ["+reverseArray([1,2,3,4])+"]");
-    console.log("Reverse In place "+reverseArrayInPlace([1,2,3,4]))
- 
-    /** Unit Testing */
-    describe("W2d2JS Practice",function(){
-        /** Reverse Array */
-        it("reverseArray([1,2,3,4]) should return [4,3,2,1]",function(){
-             assert.equal(reverseArray([1,2,3,4]).toString(),[4,3,2,1].toString())
-        })
- 
-        /** Reverse Array Inplace */
-        it("reverseArrayInPlace([1,2,3,4]) should return [4,3,2,1]",function(){
-             assert.equal(reverseArrayInPlace([1,2,3,4]).toString(),[4,3,2,1].toString())
-        })
- 
-        it("listToArray([1,2,3]) should return { value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }",function(){
-            assert.equal(listToArray([1,2,3]).toString(),"[object Object]")
-        })
- 
-        it("deepEqual(user,user1) should return true",function(){
-            assert.equal(deepEqual(user,user1).apply(),true)
-        })
-    })
- }
+  
+    console.log(recNth({
+      value: 1,
+      rest: {
+        value: 2,
+        rest: {
+          value: 3,
+          rest: null
+        }
+      }
+  
+    }, 0));
+    console.log(nth({
+      value: 1,
+      rest: {
+        value: 2,
+        rest: {
+          value: 3,
+          rest: null
+        }
+      }
+  
+    }), 0);
+  
+    describe("nth", function () {
+      it("nth({\n" +
+        "    value: 1,\n" +
+        "    rest: {\n" +
+        "      value: 2,\n" +
+        "      rest: {\n" +
+        "        value: 3,\n" +
+        "        rest: null\n" +
+        "      }\n" +
+        "    }\n" +
+        "\n" +
+        "  }, 0) -> 1", function () {
+        assert.equal(nth({
+          value: 1,
+          rest: {
+            value: 2,
+            rest: {
+              value: 3,
+              rest: null
+            }
+          }
+  
+        },0), 1)
+      })
+  
+      it("recNth({\n" +
+        "    value: 1,\n" +
+        "    rest: {\n" +
+        "      value: 2,\n" +
+        "      rest: {\n" +
+        "        value: 3,\n" +
+        "        rest: null\n" +
+        "      }\n" +
+        "    }\n" +
+        "\n" +
+        "  }, 0) -> 1", function () {
+        assert.equal(recNth({
+          value: 1,
+          rest: {
+            value: 2,
+            rest: {
+              value: 3,
+              rest: null
+            }
+          }
+  
+        }, 0),1)
+      })
+  
+    });
+  
+  }());
